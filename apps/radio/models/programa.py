1c503c5529066b697fbf8d4e12a2d2d1636d540c
Asafe Oliveira <asafepy@gmail.com>
2018-11-08 01:01:14 -0200

    removendo conflitos

--- apps/radio/models/programa.py
+++ apps/radio/models/programa.py
@@ -12,22 +12,13 @@ class Programa(models.Model):
 
     nome = models.CharField("Nome do programa", max_length=100)
     categoria = models.IntegerField('Categoria', choices=CATEGORIA_CHOICES)
-<<<<<<< HEAD
     radio = models.ForeignKey(Radio, verbose_name="Rádio", on_delete=models.CASCADE)
-=======
-    horario_exibicao = models.TimeField("Horário de exibição", auto_now=False, auto_now_add=False, )
->>>>>>> abe7524404d415ffa9753401acd225f39b342db8
 
     def __str__(self):
         return self.nome
         
     class Meta:
-<<<<<<< HEAD
         ordering = ['categoria']
-=======
-        ordering = ['horario_exibicao']
-
->>>>>>> abe7524404d415ffa9753401acd225f39b342db8
         verbose_name = 'Programa'
         verbose_name_plural = 'Programas'
 
